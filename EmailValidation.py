fd0be781df1569c491d79c4062ac1719e1a4ccef
Jiya Sahu <143689238+jiya-sahu@users.noreply.github.com>
2024-03-08 11:41:06 +0530

    Add files via upload
    Added first basic python project Email validator which checks weather an email is valid or not.

---
+++ EmailValidation.py
@@ -0,0 +1,33 @@
+#j@j.in , name@gamil.com 
+email = input("Enter your Eamil :")
+k,j,d=0,0,0
+if len(email)>=6:
+    if email[0].isalpha():
+        if ("@"in email )and (email.count("@")==1) :
+            if (email[-3]==".")^(email[-4]=="."):
+                for i in email:
+                    if (i.isspace()==True):
+                        k =1
+                    elif i.isalpha():
+                        if i.isupper==True :
+                            j=1
+                    elif i.isdigit():
+                        continue
+                    elif i == "_"or i=="."or i=="@":
+                        continue
+                    else:
+                        d=1
+                
+                if(k==1 or j==1 or d==1):
+                     print("wrong Email")
+                else :
+                    print("Right Email")
+            else:
+                 print("wrong Email")
+        else:
+             print("wrong Email")
+    else :
+        print("wrong Email")
+else:
+    print("wrong Email")
+print("")
